4aafb076fd980c5fa1fad6730d29c2c09667efce
jesseking96 <jesseking96@yahoo.com>
2019-06-27 07:00:02 -0500

    completed basic functionality

--- Dictionary_Searcher.py
+++ Dictionary_Searcher.py
@@ -8,8 +8,42 @@ Created on Wed Jun 26 12:39:14 2019
 
 import json
 
-with open("Dictionary.json", "r") as dict_data:
-    dict_file = json.load(dict_data)
+with open("Dictionary.json", "r") as dict_file:
+    dict_data = json.load(dict_file)
     
+
+def get_word():
+    while True:
+        word = input("Please enter a word: ")
+        if word.isalpha() == False:
+            print("Invalid input. Please try again." )
+            continue
+        return word
     
-print(dict_file)
+def get_definition(word):
+    
+    global dict_data
+    
+    if word in dict_data.keys():
+        print("\n")
+        print(dict_data[word])
+    else:
+        print("Word does not exist")
+
+def is_repeat():
+    while True:
+        repeat = input("Would you like to check another word (Y/N)? ")
+        print(repeat)
+        if repeat.upper() == "Y" or repeat.upper() == "N":
+            return repeat.upper() == "Y"
+        else:
+            print("Invalid input. Please try again. ")
+
+repeat = True
+
+while repeat:
+    
+    word = get_word()
+    get_definition(word)
+    repeat = is_repeat()
+        
