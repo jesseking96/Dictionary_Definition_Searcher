9d7833a5ce89b5e419e7fd9a625e683cf089c49b
jesseking96 <jesseking96@yahoo.com>
2019-06-27 07:33:26 -0500

    Added spelling correction suggestion using pyspellchecker

--- Dictionary_Searcher.py
+++ Dictionary_Searcher.py
@@ -8,6 +8,8 @@ Created on Wed Jun 26 12:39:14 2019
 
 import json
 
+from spellchecker import SpellChecker
+
 with open("Dictionary.json", "r") as dict_file:
     dict_data = json.load(dict_file)
     
@@ -28,7 +30,21 @@ def get_definition(word):
         print("\n")
         print(dict_data[word])
     else:
-        print("Word does not exist")
+        spell = SpellChecker()
+        print(f"Word does not exist. \n")
+        word_guess = spell.correction(word)
+        while True:
+            is_fixed = input(f"Did you mean {word_guess} (Y/N)? ")
+            if is_fixed. upper() in ["Y","N"]:
+                if is_fixed.upper() == "Y":
+                    print(dict_data[word_guess])
+                    break
+                else:
+                    print("\nUnknown word. ")
+                    break
+            else:
+                print("\nInvalid input. Try again. ")
+        
 
 def is_repeat():
     while True:
